11b543c4488008d400a37f6d7e5f07d7feb5476f
Gus3245 <Enderdragon3245@gmail.com>
2025-01-27 10:39:34 -0300

    Feat: Starting Queue, Define Structs

--- Data-Structs/queue.c
+++ Data-Structs/queue.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{
+	int value;
+	struct Node *prox;
+}node;
+
+typedef struct Queue{
+	struct Node *start_node;
+	struct Node *end_node;
+}queue;
 
 
 int main(int argc, char *argv[])
